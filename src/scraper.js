3bdaf615a271adb3742522a8dfa5a554df42800b
Matt Davis <matthewpeterdavis@gmail.com>
2024-04-30 13:34:09 +0100

    convert code to scrape gofundme urls

--- src/scraper.js
+++ src/scraper.js
@@ -1,9 +1,9 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 const router = require("express").Router();
-const { generateFilename, saveProductJson, generateTitle } = require("./utils");
+const { generateFilename } = require("./utils");
 
-const baseUrl = "https://www.amazon.com";
+const baseUrl = "https://www.gofundme.com";
 
 router.post("/scrape", async (req, res) => {
   // Get the URL from the request body
@@ -11,6 +11,9 @@ router.post("/scrape", async (req, res) => {
   // Validate the URL
   if (!url.includes(baseUrl)) {
     console.log("Invalid URL");
+    res.status(400).json({
+      message: "Error scraping details",
+    })
     return;
   }
 
@@ -19,30 +22,16 @@ router.post("/scrape", async (req, res) => {
     axios.get(url).then((response) => {
       // Load the HTML into cheerio
       const $ = cheerio.load(response.data); // Cheerio takes the HTML and parses it into a format that is easy to use
-      // Create an empty array to store the products
-      const products = [];
-      // Loop through each product on the page
-      $(".s-result-item").each((i, el) => {
-        const product = $(el);
 
-        const priceWhole = product.find(".a-price-whole").text();
-        const priceFraction = product.find(".a-price-fraction").text();
-        const price = priceWhole + priceFraction;
-        const link = product.find(".a-link-normal.a-text-normal").attr("href");
-        const title = generateTitle(product, link);
+      const progressString = $("div.hrt-disp-inline").text().replace(/[^0-9]/g, '')
+      const currency = $("div.hrt-disp-inline").text().replace(/[0-9,]/g, '')
+      const targetString = $("span.hrt-text-body-sm").text().replace(/[^0-9]/g, '')
 
-        // If both title, price and link are not empty, add to products array
-        if (title !== "" && price !== "" && link !== "") {
-          products.push({ title, price, link});
-        }
-      });
-      // Call the saveProductJson function to save the products array to a JSON file
-      saveProductJson(products);
-
-      // return a success message with the number of products scraped and the filename
       res.json({
-        products_saved: products.length,
-        message: "Products scraped successfully",
+        progressString,
+        targetString,
+        currency,
+        message: "Details scraped successfully",
         filename: generateFilename(),
       });
     });
